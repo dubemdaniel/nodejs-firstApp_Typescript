84d1a7110724565b0e6657beb9f7c7fc4a343523
Daniel Chukwudubem <princedaniel7890@gmail.com>
2025-08-30 18:33:21 +0100

    added views page

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 router.get('/add-product',(req, res, next) => {
     // console.log('This is a product page')
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add PRoduct</button></form>')
+    // res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add PRoduct</button></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/product', (req, res, next) => {
     // console.log('this is the product info')
+    console.log(req.body.name)
+    // if (!req.body.name) {
+    //     return res.status(400).json({error: "something went wrong"})
+    // }
     console.log(req.body)
     res.redirect('/')
 })
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
 
 
 router.get('/',(req, res, next) => {
     // console.log('in another middleware')
-    res.send("<h1>Hello from Express</h2>")
+    // res.send("<h1>Hello from Express</h2>")
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 
 module.exports = router
